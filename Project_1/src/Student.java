e2a9d7c5154cae3a5ef859b92f5edaa009e77ec6
RADHIKA <girijeshdixit@hotmail.com>
2023-07-22 17:48:56 +0200

    pushinggggggggggggg
    Change-Id: I4f3596c8f52ad60bb879ca33a9b5232b1f852177

---
+++ Project_1/src/Student.java
@@ -0,0 +1,23 @@
+
+public class Student {
+	
+	int nidhi;
+	
+	public void abc()
+	{
+		
+		System.out.println("my first program");
+	}
+	
+	
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("main prog");
+	}
+  
+
+}
+
+
+
---
+++ test22/src/test22/testt22intogithub.java
@@ -0,0 +1,11 @@
+package test22;
+
+public class testt22intogithub {
+
+public static void main(String[] args) {
+	
+	System.out.println("hello");
+	
+}
+
+}
